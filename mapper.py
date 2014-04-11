5caafd51c70ab2a088785de73f8db4737ae425df
JoyeeCheung <joyeec9h3@gmail.com>
2014-04-11 21:45:20 +0800

    reformat

--- mapper.py
+++ mapper.py
@@ -2,25 +2,23 @@
 import sys
 from itertools import *
 
-def seq(start, end):
-	return [range(start, i) for i in range(start, end+2)]
 
-def powerset(iterable):
-	s = list(iterable)
-	return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+def seq(start, end):
+    return [range(start, i) for i in range(start, end + 2)]
+
 
 def read_input(file):
-	for line in file:
-		yield line.split()
+    for line in file:
+        yield line.split()
+
 
 def main():
-	data = read_input(sys.stdin)
-	C = [a+b for a, b in product(seq(2,4), seq(5,7))]
-	for e in data:
-		for R in C:
-			k = [e[i] for i in R]
-			print "%s|%s\t%s" % (' '.join([str(i) for i in R]), ' '.join(k), str(e[1]))
+    data = read_input(sys.stdin)
+    C = [a + b for a, b in product(seq(2, 4), seq(5, 7))]
+    for e in data:
+        for R in C:
+            k = [e[i] for i in R]
+            print "%s|%s\t%s" % (' '.join([str(i) for i in R]), ' '.join(k), e[1])
 
 if __name__ == "__main__":
-	main()
-
+    main()
--- reducer.py
+++ reducer.py
@@ -4,16 +4,17 @@ from itertools import groupby
 from operator import itemgetter
 import sys
 
+
 def read_input(file):
-	for line in file:
-		yield line.rstrip().split('\t')
+    for line in file:
+        yield line.rstrip().split('\t')
+
 
 def main():
-	data = read_input(sys.stdin)
-	for rvalue, group in groupby(data, itemgetter(0)):
- 		ids = set(count for rvalue, count in group)
- 		print "%s\t%d" % (rvalue, len(ids))	
+    data = read_input(sys.stdin)
+    for key, group in groupby(data, itemgetter(0)):
+        ids = set(uid for key, uid in group)
+        print "%s\t%d" % (key, len(ids))
 
 if __name__ == "__main__":
-	main()
-
+    main()
