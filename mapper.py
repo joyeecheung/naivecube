9b465f02be073810a2ae78024983031338411f59
JoyeeCheung <joyeec9h3@gmail.com>
2014-04-11 21:37:49 +0800

    correct lattice

--- mapper.py
+++ mapper.py
@@ -2,22 +2,25 @@
 import sys
 from itertools import *
 
+def seq(start, end):
+	return [range(start, i) for i in range(start, end+2)]
+
 def powerset(iterable):
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+	s = list(iterable)
+	return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def read_input(file):
-    for line in file:
-        yield line.split()
+	for line in file:
+		yield line.split()
 
 def main():
-    data = read_input(sys.stdin)
-    C = [i for i in powerset([2,3,4,5,6,7])]
-    for e in data:
-        for R in C:
-            k = [e[i] for i in R]
-            print "%s|%s\t%s" % (' '.join([str(i) for i in R]), ' '.join(k), str(e[1]))
+	data = read_input(sys.stdin)
+	C = [a+b for a, b in product(seq(2,4), seq(5,7))]
+	for e in data:
+		for R in C:
+			k = [e[i] for i in R]
+			print "%s|%s\t%s" % (' '.join([str(i) for i in R]), ' '.join(k), str(e[1]))
 
 if __name__ == "__main__":
-    main()
+	main()
 
